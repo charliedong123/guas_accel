16b24678b3fb6a702b152049f8139f9a8bbd40b8
charliedong123 <charlie.dong@avnet.com>
2019-02-27 17:00:18 +0800

    Add files via upload

--- func.cpp
+++ func.cpp
@@ -3,80 +3,82 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
-#
+#include "gaus_accel.hpp"
 using namespace std;
-void gusReadImage(float resultImg[16][25], float *D_Image_R, int YNum, int Xn, int Yn);
-void gusReadOriImage(float Value[16], float *d_origImage, int YNum, int i, int j );
-void calculateGaus(float temImg[16][25],float temImg_1[16][25],float resultImg[16][25], float gauslut[325], float Value[16], int Offset_X, int Offset_Y );
-void gusWriteImage(float temImg_1[16][25], float *D_Image_R, int YNum,  int Xn, int Yn);
 
-void gusReadImage(float resultImg[16][25], float *D_Image_R, int YNum, int Xn, int Yn)
-{
-#pragma HLS  interface m_axi port=D_Image_R
-	int Resize_factor = 5;
-	gusRowLoop_1:for(int Rsj=0;Rsj<Resize_factor;Rsj++)
-	{
-		gusColoumLoop_1:for(int Rsi=0;Rsi<Resize_factor;Rsi++)
-		{
+//void gusReadOriImage(float Value[16], float *d_origImage, int YNum, int i, int j );
+//void calculateGaus(float temImg[16][25],float temImg_1[16][25],float resultImg[16][25], float gauslut[325], float Value[16], int Offset_X, int Offset_Y );
+//void gusWriteImage(float temImg_1[16][25], float *D_Image_R, int YNum,  int Xn, int Yn);
+//
+//void gusReadImage(float resultImg[16][25], float *D_Image_R, int YNum, int Xn, int Yn)
+//{
+//#pragma HLS  interface m_axi port=D_Image_R
+//	int Resize_factor = 5;
+//	gusRowLoop_1:for(int Rsj=0;Rsj<Resize_factor;Rsj++)
+//	{
+//		gusColoumLoop_1:for(int Rsi=0;Rsi<Resize_factor;Rsi++)
+//		{
+//
+//			gusZLoop_1:for(int z=0;z<16;z++)
+//			{
+//				resultImg[z][Rsj * Resize_factor + Rsi] = D_Image_R[ (Xn + Rsj -2 ) * YNum * 16 + 16 * (Yn + Rsi - 2) + z];
+//			}
+//		}
+//	}
+//
+//}
+//
+//void gusReadOriImage(float Value[16], float *d_origImage, int YNum, int i, int j )
+//{
+//	int Subpixels = 65;
+//	int Resize_factor = 5;
+//	gusGetValue:for(int z=0;z<16;z++)
+//	{
+//		Value[z] = d_origImage[i * YNum * 16 + z + j * 16];
+//	}
+//
+//}
+//
+//void calculateGaus(float temImg[16][25],float temImg_1[16][25],float resultImg[16][25], float gauslut[325], float Value[16], int Offset_X, int Offset_Y )
+//{
+//	int Subpixels = 65;
+//	int Resize_factor = 5;
+//	gusRowLoop_2:for(int Rsj=0;Rsj<Resize_factor;Rsj++)
+//	{
+//		gusColoumLoop_2:for(int Rsi=0;Rsi<Resize_factor;Rsi++)
+//		{
+//
+//			gusZLoop_2:for(int z=0;z<16;z++)
+//			{
+//#pragma HLS unroll
+//				temImg[z][ Rsi * Resize_factor + Rsj ] = gauslut[  Rsi * Subpixels + Offset_X ] * Value[z];
+//				temImg_1[z][ Rsj * Resize_factor + Rsi ] = resultImg[z][Rsj * Resize_factor + Rsi] + gauslut[  Rsi * Subpixels + Offset_Y ] *  temImg[z][ Rsj * Resize_factor + Rsi ];
+//			}
+//		}
+//	}
+//
+//}
+//
+//void gusWriteImage(float temImg_1[16][25], float *D_Image_R, int YNum,  int Xn, int Yn)
+//{
+//	int Resize_factor = 5;
+//	gusRowLoop_3:for(int Rsj=0;Rsj<Resize_factor;Rsj++)
+//	{
+//		gusColoumLoop_3:for(int Rsi=0;Rsi<Resize_factor;Rsi++)
+//		{
+//
+//			gusZLoop_3:for(int z=0;z<16;z++)
+//			{
+//				D_Image_R[ (Xn + Rsj -2 ) * YNum * 16 + 16 * (Yn + Rsi - 2) + z] = temImg_1[z][Rsj * Resize_factor + Rsi];
+//			}
+//		}
+//	}
+//}
 
-			gusZLoop_1:for(int z=0;z<16;z++)
-			{
-				resultImg[z][Rsj * Resize_factor + Rsi] = D_Image_R[ (Xn + Rsj -2 ) * YNum * 16 + 16 * (Yn + Rsi - 2) + z];
-			}
-		}
-	}
 
-}
+//void Gaussian_ImageRotation(float* d_origImage, float fi, int XNum, int YNum, int ZNum, int iZOffset, float* d_gausLut, float* D_Image_R)
+void Gaussian_ImageRotation(float* d_origImage, float fi, float* d_gausLut, float* D_Image_R)
 
-void gusReadOriImage(float Value[16], float *d_origImage, int YNum, int i, int j )
-{
-	int Subpixels = 65;
-	int Resize_factor = 5;
-	gusGetValue:for(int z=0;z<16;z++)
-	{
-		Value[z] = d_origImage[i * YNum * 16 + z + j * 16];
-	}
-
-}
-
-void calculateGaus(float temImg[16][25],float temImg_1[16][25],float resultImg[16][25], float gauslut[325], float Value[16], int Offset_X, int Offset_Y )
-{
-	int Subpixels = 65;
-	int Resize_factor = 5;
-	gusRowLoop_2:for(int Rsj=0;Rsj<Resize_factor;Rsj++)
-	{
-		gusColoumLoop_2:for(int Rsi=0;Rsi<Resize_factor;Rsi++)
-		{
-
-			gusZLoop_2:for(int z=0;z<16;z++)
-			{
-#pragma HLS unroll
-				temImg[z][ Rsi * Resize_factor + Rsj ] = gauslut[  Rsi * Subpixels + Offset_X ] * Value[z];
-				temImg_1[z][ Rsj * Resize_factor + Rsi ] = resultImg[z][Rsj * Resize_factor + Rsi] + gauslut[  Rsi * Subpixels + Offset_Y ] *  temImg[z][ Rsj * Resize_factor + Rsi ];
-			}
-		}
-	}
-
-}
-
-void gusWriteImage(float temImg_1[16][25], float *D_Image_R, int YNum,  int Xn, int Yn)
-{
-	int Resize_factor = 5;
-	gusRowLoop_3:for(int Rsj=0;Rsj<Resize_factor;Rsj++)
-	{
-		gusColoumLoop_3:for(int Rsi=0;Rsi<Resize_factor;Rsi++)
-		{
-
-			gusZLoop_3:for(int z=0;z<16;z++)
-			{
-				D_Image_R[ (Xn + Rsj -2 ) * YNum * 16 + 16 * (Yn + Rsi - 2) + z] = temImg_1[z][Rsj * Resize_factor + Rsi];
-			}
-		}
-	}
-}
-
-
-void Gaussian_ImageRotation(float* d_origImage, float fi, int XNum, int YNum, int ZNum, int iZOffset, float* d_gausLut, float* D_Image_R)
 {
  #pragma HLS  interface m_axi port=d_origImage
 #pragma HLS  interface m_axi port=d_gausLut
@@ -87,16 +89,16 @@ void Gaussian_ImageRotation(float* d_origImage, float fi, int XNum, int YNum, in
 	float x,y,xn,yn;
 	int Xn, Yn, Offset_X, Offset_Y, X_half, Y_half;
 
-	float Value[16];
+	float Value[672];
 #pragma HLS array_partition variable=Value complete dim=0
 	float COS,SIN;
 
 	int Resize_factor = 5;
-	float resultImg[16][25];
+	float resultImg[672];
 #pragma HLS array_partition variable=resultImg complete dim=1
-	float temImg[16][25];
+	float temImg[672];
 #pragma HLS array_partition variable=temImg complete dim=1
-	float temImg_1[16][25];
+	float temImg_1[672];
 #pragma HLS array_partition variable=temImg_1 complete dim=1
 	float temvalue[16][25];
 #pragma HLS array_partition variable=temvalue complete dim=1
@@ -133,64 +135,55 @@ void Gaussian_ImageRotation(float* d_origImage, float fi, int XNum, int YNum, in
 			Xn = Xn + X_half;
 			Yn = Yn + Y_half;
 
-			regionDataflow: {
-#pragma HLS pipeline
-				gusReadImage(resultImg, D_Image_R, YNum, Xn, Yn);
-				gusReadOriImage(Value, d_origImage, YNum, i, j);
-				calculateGaus(temImg,temImg_1,resultImg, gauslut, Value,Offset_X,Offset_Y);
-				gusWriteImage(temImg_1,D_Image_R,YNum, Xn,Yn);
-			}
-
 //			regionDataflow: {
 //#pragma HLS pipeline
-////read the result picture image 5*5 value
-//				gusRowLoop_1:for(Rsj=0;Rsj<Resize_factor;Rsj++)
-//				{
-//					gusColoumLoop_1:for(Rsi=0;Rsi<Resize_factor;Rsi++)
-//					{
-//
-//						gusZLoop_1:for(int z=0;z<16;z++)
-//						{
-//							resultImg[z][Rsj * Resize_factor + Rsi] = D_Image_R[ (Xn + Rsj -2 ) * YNum * 16 + 16 * (Yn + Rsi - 2) + z];
-//						}
-//					}
-//				}
-//// read the original picture pixel value based z axis from 0 -- 15
-//				gusGetValue:for(int z=0;z<16;z++)
-//				{
-//					Value[z] = d_origImage[i * YNum * 16 + z + j * 16];
-//				}
-//
-////calculate the gaus
-//				gusRowLoop_2:for(Rsj=0;Rsj<Resize_factor;Rsj++)
-//				{
-//					gusColoumLoop_2:for(Rsi=0;Rsi<Resize_factor;Rsi++)
-//					{
-//
-//						gusZLoop_2:for(int z=0;z<16;z++)
-//						{
-//#pragma HLS unroll
-//							temImg[z][ Rsi * Resize_factor + Rsj ] = gauslut[  Rsi * Subpixels + Offset_X ] * Value[z];
-//							temImg_1[z][ Rsj * Resize_factor + Rsi ] = resultImg[z][Rsj * Resize_factor + Rsi] + gauslut[  Rsi * Subpixels + Offset_Y ] *  temImg[z][ Rsj * Resize_factor + Rsi ];
-//						}
-//					}
-//				}
-//
-//
-////write the calculate 5*5 matrix value to  the result picture image
-//				gusRowLoop_3:for(Rsj=0;Rsj<Resize_factor;Rsj++)
-//				{
-//					gusColoumLoop_3:for(Rsi=0;Rsi<Resize_factor;Rsi++)
-//					{
-//
-//						gusZLoop_3:for(int z=0;z<16;z++)
-//						{
-//							D_Image_R[ (Xn + Rsj -2 ) * YNum * 16 + 16 * (Yn + Rsi - 2) + z] = temImg_1[z][Rsj * Resize_factor + Rsi];
-//						}
-//					}
-//				}
+//				gusReadImage(resultImg, D_Image_R, YNum, Xn, Yn);
+//				gusReadOriImage(Value, d_origImage, YNum, i, j);
+//				calculateGaus(temImg,temImg_1,resultImg, gauslut, Value,Offset_X,Offset_Y);
+//				gusWriteImage(temImg_1,D_Image_R,YNum, Xn,Yn);
 //			}
+
+
+// read the original picture pixel value based z axis from 0 -- 15
+			gusGetValue:for(int z=0;z<ZNum;z++)
+			{
+#pragma HLS LOOP_TRIPCOUNT min= 24 max= 672
+				Value[z] = d_origImage[i * YNum * ZNum + z + j * ZNum];
+			}
+			regionDataflow: {
+			//read the result picture image 5*5 value
+				gusRowLoop_1:for(Rsj=0;Rsj<Resize_factor;Rsj++)
+				{
+					gusColoumLoop_1:for(Rsi=0;Rsi<Resize_factor;Rsi++)
+					{
+#pragma HLS pipeline
+						gusZLoop_1:for(int z=0;z<672;z++)
+						{
+
+#pragma HLS LOOP_TRIPCOUNT min= 24 max= 672
+							resultImg[z] = D_Image_R[ (Xn + Rsj -2 ) * YNum * ZNum + ZNum * (Yn + Rsi - 2) + z];
+//							temImg[z] = gauslut[  Rsi * Subpixels + Offset_X ] * Value[z];
+//							temImg_1[z] = resultImg[z] + gauslut[  Rsi * Subpixels + Offset_Y ] *  temImg[z];
+//							D_Image_R[ (Xn + Rsj -2 ) * YNum * 16 + 16 * (Yn + Rsi - 2) + z] = temImg_1[z];
+						}
+//						gusZLoop_2:for(int z=0;z<672;z++)
+//						{
+//#pragma HLS LOOP_TRIPCOUNT min= 24 max= 672
+//							temImg[z] = gauslut[  Rsi * Subpixels + Offset_X ] * Value[z];
+//							temImg_1[z] = resultImg[z] + gauslut[  Rsi * Subpixels + Offset_Y ] *  temImg[z];
+//						}
+						gusZLoop_3:for(int z=0;z<672;z++)
+						{
+#pragma HLS LOOP_TRIPCOUNT min= 24 max= 672
+							D_Image_R[ (Xn + Rsj -2 ) * YNum * ZNum + ZNum * (Yn + Rsi - 2) + z] = resultImg[z] + 1;
+						}
+
+
+					}
+				}
+			}
 		}
 	}
+
 }
 
---
+++ gaus_accel.hpp
@@ -0,0 +1,7 @@
+#define RO  1 // Resource Optimized (8-pixel implementation)
+#define NO  0 // Normal Operation (1-pixel implementation)
+
+#define XNum  	256
+#define YNum 	256
+#define ZNum 	672
+
